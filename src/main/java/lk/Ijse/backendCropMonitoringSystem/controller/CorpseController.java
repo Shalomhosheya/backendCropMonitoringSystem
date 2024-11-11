cd5acc1dd0691647c84c494c3bee64bc1efcf38b
KD ShalomHosheya Jayathilaka <129625075+Shalomhosheya@users.noreply.github.com>
2024-11-11 22:22:41 +0530

    "ER build"

--- src/main/java/lk/Ijse/backendCropMonitoringSystem/controller/CorpseController.java
+++ src/main/java/lk/Ijse/backendCropMonitoringSystem/controller/CorpseController.java
@@ -1,5 +1,7 @@
 package lk.Ijse.backendCropMonitoringSystem.controller;
 
+import lk.Ijse.backendCropMonitoringSystem.service.CorpseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,4 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/vi/corpse")
 @CrossOrigin
 public class CorpseController {
+    @Autowired
+    private CorpseService corpseService;
 }
--- src/main/java/lk/Ijse/backendCropMonitoringSystem/service/impl/CorpseServiceImpl.java
+++ src/main/java/lk/Ijse/backendCropMonitoringSystem/service/impl/CorpseServiceImpl.java
@@ -1,6 +1,10 @@
 package lk.Ijse.backendCropMonitoringSystem.service.impl;
 
+import jakarta.transaction.Transactional;
 import lk.Ijse.backendCropMonitoringSystem.service.CorpseService;
+import org.springframework.stereotype.Service;
 
+@Service
+@Transactional
 public class CorpseServiceImpl implements CorpseService {
 }
